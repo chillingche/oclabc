2be7554cbe850acd5bc6289926f0fbed095efc3c
chillingche <zimar.wang@gmail.com>
2022-02-14 01:35:23 +0800

    add a53 gflops example

---
+++ examples/gflops.cpp
@@ -0,0 +1,245 @@
+#include <sys/syscall.h>
+#include <sys/time.h>
+#include <sched.h>
+#include <stdint.h>
+#include <unistd.h>
+#include "log.h"
+
+inline int set_thread_affinity(int num)
+{
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(num, &mask);
+    int status = syscall(__NR_sched_setaffinity, gettid(), sizeof(mask), &mask);
+    if (status) {
+        LOGE("fail to set affinity %d\n", status);
+        return -1;
+    }
+    return 0;
+}
+
+void mvm_row_kernel(uint32_t N, uint32_t K, float *matrix, float *vector, float *result)
+{
+    int KTail = K % 4;
+    int KInner = K - KTail;
+    float *w0 = matrix;
+    float *w1 = matrix + K * N;
+    float *w2 = matrix + K * 2 * N;
+    float *w3 = matrix + K * 3 * N;
+    asm volatile("mov x19, %5\n"
+                 "ld1 {v18.s}[0], [x19]\n"
+                 "add x19, x19, %8\n"
+                 "ld1 {v18.s}[1], [x19]\n"
+                 "add x19, x19, %8\n"
+                 "ld1 {v18.s}[2], [x19]\n"
+                 "add x19, x19, %8\n"
+                 "ld1 {v18.s}[3], [x19]\n"
+
+                 "movi v17.4s, #0x0\n"
+                 "movi v16.4s, #0x0\n"
+                 "movi v9.4s, #0x0\n"
+                 "movi v10.4s, #0x0\n"
+                 "movi v11.4s, #0x0\n"
+                 "movi v12.4s, #0x0\n"
+                 "mov x20, %6\n"
+                 "cmp x20, #0x0\n"
+                 "beq 3f\n"
+                 "0:\n"
+
+                 "ld1 {v0.4s}, [%0], #16\n"
+                 "ld1 {v1.4s}, [%1], #16\n"
+                 "ld1 {v2.4s}, [%2], #16\n"
+                 "ld1 {v3.4s}, [%3], #16\n"
+                 "ld1 {v4.4s}, [%4], #16\n"
+
+                 "fmla v9.4s,  v1.4s, v0.4s\n"
+                 "fmla v10.4s,  v2.4s, v0.4s\n"
+                 "fmla v11.4s,  v3.4s, v0.4s\n"
+                 "fmla v12.4s,  v4.4s, v0.4s\n"
+
+                 "subs x20, x20, #4\n"
+                 "bne 0b\n"
+
+                 "faddp v13.4s,  v9.4s, v10.4s\n"
+                 "faddp v14.4s, v11.4s, v12.4s\n"
+                 "faddp v17.4s, v13.4s, v14.4s\n"
+                 "3:\n"
+                 "mov x16, %7\n"
+                 "cmp x16, #0x0\n"
+                 "beq 2f\n"
+
+                 "1:\n"
+                 "ld1 {v8.s}[0], [%0], #4\n"
+
+                 "ld1 {v1.s}[0], [%1], #4\n"
+                 "ld1 {v1.s}[1], [%2], #4\n"
+                 "ld1 {v1.s}[2], [%3], #4\n"
+                 "ld1 {v1.s}[3], [%4], #4\n"
+                 "fmla v16.4s,  v1.4s, v8.s[0]\n"
+
+                 "subs x16, x16, 0x1\n"
+                 "bne 1b\n"
+
+                 "fadd v17.4s, v17.4s, v16.4s\n"
+
+                 "2:\n"
+
+                 "fadd v17.4s, v17.4s, v18.4s\n"
+                 "mov x19, %5\n"
+                 "st1 {v17.s}[0], [x19]\n"
+                 "add x19, x19, %8\n"
+                 "st1 {v17.s}[1], [x19]\n"
+                 "add x19, x19, %8\n"
+                 "st1 {v17.s}[2], [x19]\n"
+                 "add x19, x19, %8\n"
+                 "st1 {v17.s}[3], [x19]\n"
+                 : "+r"(vector), "+r"(w0), "+r"(w1), "+r"(w2), "+r"(w3), "+r"(result)
+                 : "r"((int64_t)KInner), "r"((int64_t)KTail), "r"((int64_t)N * 4)
+                 : "memory", "cc", "x19", "x20", "x21", "x22", "x23", "x24", "x15", "x16", "v0",
+                 "v1", "v2", "v3", "v4", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v16",
+                 "v17", "v18");
+}
+
+void freefma(int64_t loop)
+{
+    asm volatile(
+        "mov x9, %0\n"
+        "0:\n"
+        "fmla v0.4s,  v12.4s, v13.s[0]\n"
+        "fmla v1.4s,  v12.4s, v13.s[1]\n"
+        "fmla v2.4s,  v12.4s, v13.s[2]\n"
+        "fmla v3.4s,  v12.4s, v13.s[3]\n"
+        "subs x9, x9, #1\n"
+        "fmla v4.4s,  v12.4s, v13.s[0]\n"
+        "bne 0b\n"
+        :
+        : "r"(loop)
+        : "memory", "cc", "x9", "v0", "v1", "v2", "v3", "v4",
+          "v12", "v13");
+}
+
+int main(int argc, char const *argv[])
+{
+    int64_t loop = 1024 * 1024 * 1024;
+    float macs = loop * 5 * (4.0f + 4.0f);
+    {
+        set_thread_affinity(0);
+        timeval begin, end;
+        gettimeofday(&begin, NULL);
+        freefma(loop);
+        gettimeofday(&end, NULL);
+        float lt = (end.tv_sec - begin.tv_sec) * 1000.0f +
+                   (end.tv_usec - begin.tv_usec) / 1000.0f;
+        float gfp = macs / lt * 1000.0f / 1000.0f /
+                    1000.0f / 1000.0f;
+        LOGI("time elapsed: %.4f ms", lt);
+        LOGI("GFLOPS: %.4f", gfp);
+    }
+
+    {
+        set_thread_affinity(1);
+        timeval begin, end;
+        gettimeofday(&begin, NULL);
+        freefma(loop);
+        gettimeofday(&end, NULL);
+        float lt = (end.tv_sec - begin.tv_sec) * 1000.0f +
+                   (end.tv_usec - begin.tv_usec) / 1000.0f;
+        float gfp = macs / lt * 1000.0f / 1000.0f /
+                    1000.0f / 1000.0f;
+        LOGI("time elapsed: %.4f ms", lt);
+        LOGI("GFLOPS: %.4f", gfp);
+    }
+
+    {
+        set_thread_affinity(2);
+        
+        timeval begin, end;
+        gettimeofday(&begin, NULL);
+        freefma(loop);
+        gettimeofday(&end, NULL);
+        float lt = (end.tv_sec - begin.tv_sec) * 1000.0f +
+                   (end.tv_usec - begin.tv_usec) / 1000.0f;
+        float gfp = macs / lt * 1000.0f / 1000.0f /
+                    1000.0f / 1000.0f;
+        LOGI("time elapsed: %.4f ms", lt);
+        LOGI("GFLOPS: %.4f", gfp);
+    }
+
+    {
+        set_thread_affinity(3);
+        
+        timeval begin, end;
+        gettimeofday(&begin, NULL);
+        freefma(loop);
+        gettimeofday(&end, NULL);
+        float lt = (end.tv_sec - begin.tv_sec) * 1000.0f +
+                   (end.tv_usec - begin.tv_usec) / 1000.0f;
+        float gfp = macs / lt * 1000.0f / 1000.0f /
+                    1000.0f / 1000.0f;
+        LOGI("time elapsed: %.4f ms", lt);
+        LOGI("GFLOPS: %.4f", gfp);
+    }
+
+    {
+        set_thread_affinity(4);
+        
+        timeval begin, end;
+        gettimeofday(&begin, NULL);
+        freefma(loop);
+        gettimeofday(&end, NULL);
+        float lt = (end.tv_sec - begin.tv_sec) * 1000.0f +
+                   (end.tv_usec - begin.tv_usec) / 1000.0f;
+        float gfp = macs / lt * 1000.0f / 1000.0f /
+                    1000.0f / 1000.0f;
+        LOGI("time elapsed: %.4f ms", lt);
+        LOGI("GFLOPS: %.4f", gfp);
+    }
+
+    {
+        set_thread_affinity(5);
+        
+        timeval begin, end;
+        gettimeofday(&begin, NULL);
+        freefma(loop);
+        gettimeofday(&end, NULL);
+        float lt = (end.tv_sec - begin.tv_sec) * 1000.0f +
+                   (end.tv_usec - begin.tv_usec) / 1000.0f;
+        float gfp = macs / lt * 1000.0f / 1000.0f /
+                    1000.0f / 1000.0f;
+        LOGI("time elapsed: %.4f ms", lt);
+        LOGI("GFLOPS: %.4f", gfp);
+    }
+
+    {
+        set_thread_affinity(6);
+        
+        timeval begin, end;
+        gettimeofday(&begin, NULL);
+        freefma(loop);
+        gettimeofday(&end, NULL);
+        float lt = (end.tv_sec - begin.tv_sec) * 1000.0f +
+                   (end.tv_usec - begin.tv_usec) / 1000.0f;
+        float gfp = macs / lt * 1000.0f / 1000.0f /
+                    1000.0f / 1000.0f;
+        LOGI("time elapsed: %.4f ms", lt);
+        LOGI("GFLOPS: %.4f", gfp);
+    }
+
+    {
+        set_thread_affinity(7);
+        
+        timeval begin, end;
+        gettimeofday(&begin, NULL);
+        freefma(loop);
+        gettimeofday(&end, NULL);
+        float lt = (end.tv_sec - begin.tv_sec) * 1000.0f +
+                   (end.tv_usec - begin.tv_usec) / 1000.0f;
+        float gfp = macs / lt * 1000.0f / 1000.0f /
+                    1000.0f / 1000.0f;
+        LOGI("time elapsed: %.4f ms", lt);
+        LOGI("GFLOPS: %.4f", gfp);
+    }
+
+    return 0;
+}
+
